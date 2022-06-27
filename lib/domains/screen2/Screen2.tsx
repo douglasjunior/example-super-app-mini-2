9705775fe49a0b7c4f84af573e061458116abf72
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2022-06-27 17:31:28 -0300

    implementa um subscribe do EventEmitter

--- lib/domains/screen2/Screen2.tsx
+++ lib/domains/screen2/Screen2.tsx
@@ -1,20 +1,51 @@
-import {useAppContext} from 'example-super-app-context';
-import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {useAppContext, useSubscribe} from 'example-super-app-context';
+import React, {useState} from 'react';
+import {Button, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {useNavigation} from '../../hooks/useNavigation';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textBox: {
+    margin: 8,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 4,
+    padding: 8,
+  },
+});
+
+const TextBox: React.FC<{
+  children: React.ReactNode;
+}> = ({children}) => {
+  return <Text style={styles.textBox}>{children}</Text>;
+};
 
 const Screen2 = () => {
   const navigation = useNavigation();
   const {login} = useAppContext();
+  const [count, setCount] = useState(0);
+
+  useSubscribe('INCREMENT_MESSAGE', value => {
+    if (typeof value !== 'number') {
+      return;
+    }
+    setCount(previous => (previous += value));
+  });
+
   return (
-    <View style={{flex: 1}}>
-      <Button
-        title="Voltar"
-        onPress={() => {
-          navigation.goBack();
-        }}
-      />
-      <Text>{JSON.stringify(login, null, 2)}</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Button
+          title="Voltar"
+          onPress={() => {
+            navigation.goBack();
+          }}
+        />
+        <TextBox>{JSON.stringify(login, null, 2)}</TextBox>
+        <TextBox>Contador: {count}</TextBox>
+      </ScrollView>
     </View>
   );
 };
